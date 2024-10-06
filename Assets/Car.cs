06ee56843dd27761cd84a24522bc09b958853c94
wiyadathi <wiyada.t@bu.ac.th>
2024-10-06 15:16:09 +0700

    add Car class

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class Car : Vehicle
 {
-    // Start is called before the first frame update
+    int passengerCapacity;
+    public int PassengerCapacity {  get; private set; }
     void Start()
     {
-        
+        Init("Toyota", "Corolla",50, 80, 100);
+        PassengerCapacity = 5;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ReFuel(float newFuel)
     {
-        
+        Debug.Log("Car is refueled: " + newFuel +", Current fuel: " + (Fuel+newFuel));
+    }
+
+    public override void Repair()
+    {
+        throw new System.NotImplementedException();
     }
 }
